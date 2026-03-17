6f250f887a0641250459586b21f48b304218c136
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-17 18:17:45 +0200

    added multipage bg image

--- src/components/websites-page/multipage/multipage.tsx
+++ src/components/websites-page/multipage/multipage.tsx
@@ -1,5 +1,6 @@
 import SiteTypeSection from "@/components/ui/site-type-section";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 
 export default function Multipage() {
   const t = useTranslations("WebSitesPage.Multipage");
@@ -13,17 +14,29 @@ export default function Multipage() {
   ];
 
   return (
-    <SiteTypeSection
-      title={t.rich("title", {
-        gray: (chunks) => <span className="text-[#818181]">{chunks}</span>,
-      })}
-      description={t("description")}
-      imageUrl={"/websites-multipage.webp"}
-      imageAlt="Landing page"
-      list={list}
-      buttonText={t("button")}
-      subtitle={t("subtitle")}
-      variant="right"
-    />
+    <div className="relative">
+      <div className="absolute -z-10 top-[20px] left-[-195px] lg:top-[190px] lg:right-[-480px] w-[1136px] ">
+        <Image
+          width={1136}
+          height={1053}
+          src="/websites-multipage-shadow.webp"
+          alt="websites-multipage-shadow"
+          sizes="1136px"
+          className="select-none pointer-events-none"
+        />
+      </div>
+      <SiteTypeSection
+        title={t.rich("title", {
+          gray: (chunks) => <span className="text-[#818181]">{chunks}</span>,
+        })}
+        description={t("description")}
+        imageUrl={"/websites-multipage.webp"}
+        imageAlt="Landing page"
+        list={list}
+        buttonText={t("button")}
+        subtitle={t("subtitle")}
+        variant="right"
+      />
+    </div>
   );
 }
