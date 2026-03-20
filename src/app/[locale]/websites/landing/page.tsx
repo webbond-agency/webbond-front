dd317091f60d34c35e1c8f38ffe82481fd72c215
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-20 20:07:31 +0200

    added faq section

--- src/app/[locale]/websites/landing/page.tsx
+++ src/app/[locale]/websites/landing/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/landing-page/hero/hero";
+import Faq from "@/components/landing-page/faq/faq";
 
 export default function LandingPage() {
   return (
     <div className="pt-15 sm:pt-20 md:pt-[110px]">
       <Hero />
+      <Faq />
     </div>
   );
 }
