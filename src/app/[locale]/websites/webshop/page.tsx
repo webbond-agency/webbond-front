c82f87e15333624d5d4d7b1ebfe638e80074bd08
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-20 19:44:07 +0200

    added texts

---
+++ src/app/[locale]/websites/webshop/page.tsx
@@ -0,0 +1,9 @@
+import Hero from "@/components/webshop-page/hero/hero";
+
+export default function LandingPage() {
+  return (
+    <div className="pt-15 sm:pt-20 md:pt-[110px]">
+      <Hero />
+    </div>
+  );
+}
