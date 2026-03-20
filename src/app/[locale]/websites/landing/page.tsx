9d8fc9ce4e3de67e59ab54c7496b1950c5b5381a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-20 15:21:33 +0200

    added texts

---
+++ src/app/[locale]/websites/landing/page.tsx
@@ -0,0 +1,3 @@
+export default function LandingPage() {
+  return <div className="pt-15 sm:pt-20 md:pt-[110px]"></div>;
+}
---
+++ src/components/landing-page/hero/hero.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/ui/container";
+
+export default function Hero() {
+  return (
+    <section className="pt-18 sm:pt-[52px] md:pt-[50px] pb-30 lg:pb-[105px]">
+      <Container></Container>
+    </section>
+  );
+}
