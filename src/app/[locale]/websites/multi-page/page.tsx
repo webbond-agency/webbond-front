058da5de4c6c3f19439f4037678db7334dfecaec
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-22 17:59:56 +0200

    added faq section

--- src/app/[locale]/websites/multi-page/page.tsx
+++ src/app/[locale]/websites/multi-page/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/multipage-page/hero/hero";
+import Faq from "@/components/multipage-page/faq/faq";
 
 export default function MultiPagePage() {
   return (
     <div className="pt-15 sm:pt-20 md:pt-[110px]">
       <Hero />
+      <Faq />
     </div>
   );
 }
