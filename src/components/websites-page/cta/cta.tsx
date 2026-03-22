bfb4eaeda0572cc2fe92010b537c35d0521666d2
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-22 20:12:56 +0200

    updated styles

--- src/components/websites-page/cta/cta.tsx
+++ src/components/websites-page/cta/cta.tsx
@@ -3,19 +3,16 @@
 import { useTranslations } from "next-intl";
 
 import CtaWrapper from "@/components/shared/cta/cta-wrapper";
-import Container from "@/components/ui/container";
 
 export default function Cta() {
   const t = useTranslations("WebSitesPage.Cta");
 
   return (
-    <Container>
-      <CtaWrapper
-        title={t("title")}
-        description={t("description")}
-        buttonText={t("button")}
-        className="pt-0 pb-25 lg:pb-[273px]"
-      />
-    </Container>
+    <CtaWrapper
+      title={t("title")}
+      description={t("description")}
+      buttonText={t("button")}
+      className="pt-0 pb-25 lg:pb-[273px]"
+    />
   );
 }
