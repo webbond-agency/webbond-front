d26d2f66e79553724f7efae34d37a1a5ffb47166
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-23 14:48:31 +0200

    added order webshop section

--- src/app/[locale]/websites/webshop/page.tsx
+++ src/app/[locale]/websites/webshop/page.tsx
@@ -2,11 +2,13 @@ import Hero from "@/components/webshop-page/hero/hero";
 import Faq from "@/components/webshop-page/faq/faq";
 import Cta from "@/components/webshop-page/cta/cta";
 import ContactsContainer from "@/components/contacts/contacts-container";
+import OrderWebshop from "@/components/webshop-page/order-webshop/order-webshop";
 
 export default function LandingPage() {
   return (
     <div className="pt-15 sm:pt-20 md:pt-[110px]">
       <Hero />
+      <OrderWebshop />
       <Faq />
       <Cta />
       <ContactsContainer />
