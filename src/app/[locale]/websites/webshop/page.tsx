1c4a9854ae75ec9b015d51c5f7bf5010e86720ed
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-23 13:44:15 +0200

    added cta section

--- src/app/[locale]/websites/webshop/page.tsx
+++ src/app/[locale]/websites/webshop/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/webshop-page/hero/hero";
 import Faq from "@/components/webshop-page/faq/faq";
+import Cta from "@/components/webshop-page/cta/cta";
 
 export default function LandingPage() {
   return (
     <div className="pt-15 sm:pt-20 md:pt-[110px]">
       <Hero />
       <Faq />
+      <Cta />
     </div>
   );
 }
