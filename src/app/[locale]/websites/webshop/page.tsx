ec8df528527e69910a9915cbe728fbe38a6febff
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-23 17:23:20 +0200

    added services webshop

--- src/app/[locale]/websites/webshop/page.tsx
+++ src/app/[locale]/websites/webshop/page.tsx
@@ -3,11 +3,13 @@ import Faq from "@/components/webshop-page/faq/faq";
 import Cta from "@/components/webshop-page/cta/cta";
 import ContactsContainer from "@/components/contacts/contacts-container";
 import OrderWebshop from "@/components/webshop-page/order-webshop/order-webshop";
+import ServicesWebshop from "@/components/webshop-page/services-webshop/services-webshop";
 
 export default function LandingPage() {
   return (
     <div className="pt-15 sm:pt-20 md:pt-[110px]">
       <Hero />
+      <ServicesWebshop />
       <OrderWebshop />
       <Faq />
       <Cta />
