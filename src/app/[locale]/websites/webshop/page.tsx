1f628992fb84358c4d7aef8f66d4e220b31e38e0
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-24 18:42:01 +0200

    added webshop packages section

--- src/app/[locale]/websites/webshop/page.tsx
+++ src/app/[locale]/websites/webshop/page.tsx
@@ -4,11 +4,13 @@ import Cta from "@/components/webshop-page/cta/cta";
 import ContactsContainer from "@/components/contacts/contacts-container";
 import OrderWebshop from "@/components/webshop-page/order-webshop/order-webshop";
 import ServicesWebshop from "@/components/webshop-page/services-webshop/services-webshop";
+import Packages from "@/components/webshop-page/packages/packages";
 
-export default function LandingPage() {
+export default function WebshopPage() {
   return (
     <div className="pt-15 sm:pt-20 md:pt-[110px]">
       <Hero />
+      <Packages />
       <ServicesWebshop />
       <OrderWebshop />
       <Faq />
