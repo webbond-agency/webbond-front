c5bfb1dbc36585fb77a7cdc66d21a636f41065c2
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-25 17:46:04 +0200

    added new pages

---
+++ src/app/[locale]/blog/page.tsx
@@ -0,0 +1,3 @@
+export default function BlogPage() {
+  return <div className="pt-15 sm:pt-20 md:pt-[110px] overflow-y-clip"></div>;
+}
---
+++ src/app/[locale]/price/page.tsx
@@ -0,0 +1,3 @@
+export default function PricePage() {
+  return <div className="pt-15 sm:pt-20 md:pt-[110px] overflow-y-clip"></div>;
+}
