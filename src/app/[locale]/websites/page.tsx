00f0ba2c6c8d4234e4774444dff6da3b198c4543
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-25 15:49:32 +0200

    updated styles

--- src/app/[locale]/websites/page.tsx
+++ src/app/[locale]/websites/page.tsx
@@ -16,7 +16,7 @@ export default async function WebSitesPage() {
     { label: t("websites") },
   ];
   return (
-    <div className="pt-15 sm:pt-20 md:pt-[110px]">
+    <div className="pt-15 sm:pt-20 md:pt-[110px] overflow-y-clip">
       <Hero />
       <Breadcrumbs steps={breadcrumbSteps} className="py-10 lg:pt-0 lg:pb-20" />
       <Landing />
