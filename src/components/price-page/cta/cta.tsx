558a2da1680c1831fe7b90c4a0e3c2cb5fd1b1fb
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-27 19:25:09 +0200

    added texts

---
+++ src/components/price-page/cta/cta.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/ui/container";
+
+export default function Cta() {
+  return (
+    <section className="pb-[148px] lg:pb-[192px]">
+      <Container></Container>
+    </section>
+  );
+}
