18a3043be1171a8028271771928319fbaeb68a38
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-27 16:09:01 +0200

    added hero section

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/blog-page/hero/hero";
+
 export default function BlogPage() {
-  return <div className="pt-15 sm:pt-20 md:pt-[110px] overflow-y-clip"></div>;
+  return (
+    <div className="pt-15 sm:pt-20 md:pt-[110px] overflow-y-clip">
+      <Hero />{" "}
+    </div>
+  );
 }
