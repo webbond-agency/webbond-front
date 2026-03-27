ec9cd54e6121635dcc66b5ad8047afd3a102e2f8
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-27 16:29:13 +0200

    added texts

--- src/app/[locale]/price/page.tsx
+++ src/app/[locale]/price/page.tsx
@@ -1,3 +1,9 @@
-export default function PricePage() {
-  return <div className="pt-15 sm:pt-20 md:pt-[110px] overflow-y-clip"></div>;
+import Hero from "@/components/price-page/hero/hero";
+
+export default async function PricePage() {
+  return (
+    <div className="pt-15 sm:pt-20 md:pt-[110px] overflow-y-clip">
+      <Hero />
+    </div>
+  );
 }
