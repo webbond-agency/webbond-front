0e3c00f1d31dc3e6463944a6d899e21c0b6c2b42
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-27 17:56:09 +0200

    added faq section

--- src/app/[locale]/price/page.tsx
+++ src/app/[locale]/price/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/price-page/hero/hero";
+import Faq from "@/components/price-page/faq/faq";
 import { Breadcrumbs } from "@/components/ui/breadcrumbs";
 import { getTranslations } from "next-intl/server";
 
@@ -11,9 +12,10 @@ export default async function PricePage() {
   ];
 
   return (
-    <div className="pt-15 sm:pt-20 md:pt-[110px] overflow-y-clip">
+    <div className="pt-15 sm:pt-20 md:pt-[110px]">
       <Hero />
       <Breadcrumbs steps={breadcrumbSteps} className="py-10 lg:pt-0 lg:pb-20" />
+      <Faq />
     </div>
   );
 }
