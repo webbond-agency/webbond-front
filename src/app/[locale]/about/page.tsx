fe707804d79c0abc9807966f4fc9819fd9100df7
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-30 15:31:44 +0300

    added texts

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -3,6 +3,7 @@ import Faq from "@/components/about-page/faq/faq";
 import { Breadcrumbs } from "@/components/ui/breadcrumbs";
 import { getTranslations } from "next-intl/server";
 import Cases from "@/components/about-page/cases/cases";
+import Benefits from "@/components/about-page/benefits/benefits";
 
 export default async function AboutPage() {
   const t = await getTranslations("Breadcrumbs");
@@ -16,6 +17,7 @@ export default async function AboutPage() {
     <div className="pt-15 sm:pt-20 md:pt-[110px]">
       <Hero />
       <Breadcrumbs steps={breadcrumbSteps} className="py-10 lg:pt-0 lg:pb-20" />
+      <Benefits />
       <Cases />
       <Faq />
     </div>
---
+++ src/components/about-page/benefits/benefits.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/ui/container";
+
+export default function Benefits() {
+  return (
+    <section className="pb-[180px] lg:pb-[192px]">
+      <Container></Container>
+    </section>
+  );
+}
