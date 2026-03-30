10b41cd8e9be3f2dba0487b0ddcbf697779d0e22
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-03-30 13:50:17 +0300

    added section

---
+++ src/components/about-page/cases/cases.tsx
@@ -0,0 +1,35 @@
+import Order from "@/components/shared/order/order";
+import Container from "@/components/ui/container";
+import { useTranslations } from "next-intl";
+
+export default function Cases() {
+  const t = useTranslations("AboutPage.Cases");
+
+  return (
+    <section className="pb-[148px] lg:pb-[192px]">
+      <Container>
+        <div className="flex flex-col gap-3 md:flex-row md:items-end md:justify-between mb-6 lg:mb-12">
+          <div>
+            <h2 className="mb-3 lg:mb-4 text-[36px] lg:text-[64px] font-manrope font-light uppercase leading-[120%] text-white">
+              {t("title")}
+            </h2>
+            <p className="md:max-w-[300px] lg:max-w-[647px] font-manrope text-[16px] font-light leading-[120%] text-white lg:text-[20px] uppercase">
+              {t("descriptionLeft")}
+            </p>
+          </div>
+          <div className="flex gap-5 md:max-w-[300px] lg:max-w-[392px]">
+            <div className="w-[2px] h-auto bg-[linear-gradient(0deg,_#ffffff_0%,_#0a0704_100%)] opacity-32 rounded-full" />
+            <p className="font-montserrat text-[14px] font-light leading-[120%] text-white lg:text-[18px]">
+              {t.rich("descriptionRight", {
+                gray: (chunks) => (
+                  <span className="text-[#818181]">{chunks}</span>
+                ),
+              })}
+            </p>
+          </div>
+        </div>
+        <Order image="/about-page-cases-image.webp" buttonText={t("button")} />
+      </Container>
+    </section>
+  );
+}
