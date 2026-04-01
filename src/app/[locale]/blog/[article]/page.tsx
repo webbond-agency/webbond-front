346a34e4478711890ccbd65d451749d417e1dfbc
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-01 17:20:08 +0300

    added not found component

--- src/app/[locale]/blog/[article]/page.tsx
+++ src/app/[locale]/blog/[article]/page.tsx
@@ -2,7 +2,8 @@ import { blogPostBySlugQuery } from "@/lib/queries";
 import type { BlogPostBySlug } from "@/types/blog";
 import { fetchSanityData } from "@/utils/fetchSanityData";
 import Hero from "@/components/article-page/hero/hero";
-import { notFound } from "next/navigation";
+import { getTranslations } from "next-intl/server";
+import ArticleNotFound from "@/components/article-page/article-not-found";
 
 export default async function ArticlePage({
   params,
@@ -10,6 +11,7 @@ export default async function ArticlePage({
   params: Promise<{ article: string; locale: string }>;
 }) {
   const { article, locale } = await params;
+  const t = await getTranslations("ArticlePage.ArticleNotFound");
 
   const blogPost = await fetchSanityData<BlogPostBySlug | null>(
     blogPostBySlugQuery,
@@ -20,7 +22,7 @@ export default async function ArticlePage({
   );
 
   if (!blogPost) {
-    return <section></section>;
+    return <ArticleNotFound title={t("title")} description={t("description")} />;
   }
 
   return (
