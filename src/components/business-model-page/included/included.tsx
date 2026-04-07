566ba2e6680f4ed9c3b16604f1dc5f5c5dd4a285
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-07 16:49:56 +0300

    added bg images

--- src/components/business-model-page/included/included.tsx
+++ src/components/business-model-page/included/included.tsx
@@ -1,6 +1,7 @@
 import Container from "@/components/ui/container";
 import { useTranslations } from "next-intl";
 import Package from "./package";
+import Image from "next/image";
 
 export default function Included() {
   const t = useTranslations("BusinessModelPage.included");
@@ -11,7 +12,31 @@ export default function Included() {
 
   return (
     <section className="pb-[148px] lg:pb-[192px]">
-      <Container>
+      <Container className="relative">
+        <div className="absolute top-[295px] lg:top-[-402px] right-[-437px] lg:right-[-612px] w-[924px] h-[940px] lg:w-[1849px] lg:h-[1906px] mix-blend-hard-light">
+          <Image
+            src="/business-model-page-included-decor.webp"
+            alt="Background"
+            fill
+            className="-z-10 object-cover"
+          />
+        </div>
+        <div className="lg:hidden absolute bottom-[-85px] lg:top-[-502px] left-[-378px] lg:left-[-612px] w-[609px] h-[578px] lg:w-[1218px] lg:h-[1155px]">
+          <Image
+            src="/business-model-page-included-shadow-mob.webp"
+            alt="Background"
+            fill
+            className="-z-10 object-cover"
+          />
+        </div>
+        <div className="lg:block hidden absolute bottom-[-85px] lg:bottom-[-488px] left-[-378px] lg:left-[-773px] w-[609px] h-[578px] lg:w-[1218px] lg:h-[1155px]">
+          <Image
+            src="/business-model-page-included-shadow.webp"
+            alt="Background"
+            fill
+            className="-z-10 object-cover"
+          />
+        </div>
         <h2 className="mb-6 lg:mb-[75px] font-manrope text-[36px] lg:text-[64px] leading-[120%] text-white font-light uppercase md:text-right">
           {t.rich("title", {
             gray: (chunks) => <span className="text-[#818181]">{chunks}</span>,
