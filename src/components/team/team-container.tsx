4a761a6e677a01580bac3a35eb8d258588efa072
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-08 18:37:54 +0300

    updated team section button

--- src/components/team/team-container.tsx
+++ src/components/team/team-container.tsx
@@ -1,18 +1,10 @@
-'use client';
-import Image from 'next/image';
-import TeamSlider from './team-slider';
-import GooeyWhiteButton from '../ui/gooey-white-button';
-import { useTranslations } from 'next-intl';
-import dynamic from 'next/dynamic';
-import { useState } from 'react';
-
-const FeedbackModal = dynamic(() => import('@/components/feedback-modal'), {
-  ssr: false,
-});
+import Image from "next/image";
+import TeamSlider from "./team-slider";
+import { useTranslations } from "next-intl";
+import GooeyWhiteLink from "../ui/gooey-white-link";
 
 const TeamContainer = () => {
-  const t = useTranslations('Team');
-  const [isFeedbackOpen, setIsFeedbackOpen] = useState(false);
+  const t = useTranslations("Team");
 
   return (
     <section className="relative z-1">
@@ -26,19 +18,18 @@ const TeamContainer = () => {
       />
       <div className="px-[20px] sm:px-[40px]">
         <h2 className="max-w-[255px] sm:max-w-[400px] relative mb-[52px] text-[40px] sm:text-[48px] font-manrope font-light uppercase text-white leading-[120%] before:content-[''] before:block before:absolute before:top-1/2 before:-translate-y-1/2 before:right-[-26px] before:w-[20px] before:h-[20px] before:bg-white before:rounded-full">
-          {t('title')}
+          {t("title")}
         </h2>
       </div>
       <TeamSlider />
       <div className="px-[20px] sm:px-[40px]">
-        <GooeyWhiteButton
-          text={t('buttonQuote')}
-          onClick={() => setIsFeedbackOpen(true)}
-          className="mx-auto text-center w-full text-[14px] font-montserrat font-light text-black"
+        <GooeyWhiteLink
+          href="/about"
+          text={t("buttonAbout")}
           height={52}
+          className="mx-auto text-center w-full text-[14px] font-montserrat font-light text-black"
         />
       </div>
-      <FeedbackModal isOpen={isFeedbackOpen} onOpenChange={setIsFeedbackOpen} />
     </section>
   );
 };
