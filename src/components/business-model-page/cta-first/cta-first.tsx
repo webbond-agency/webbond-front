62e8f23def6cad028441c2af60fe3271b114fa45
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-08 16:14:30 +0300

    updated animations

--- src/components/business-model-page/cta-first/cta-first.tsx
+++ src/components/business-model-page/cta-first/cta-first.tsx
@@ -30,8 +30,8 @@ export default function CtaFirst() {
             {t("title")}
           </m.h2>
           <m.div
-            initial={{ opacity: 0, y: -30 }}
-            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, x: 30 }}
+            whileInView={{ opacity: 1, x: 0 }}
             viewport={{ once: true }}
             transition={{ duration: 0.7, delay: 0.6 }}
             className="flex md:flex-col gap-6 items-center justify-between"
